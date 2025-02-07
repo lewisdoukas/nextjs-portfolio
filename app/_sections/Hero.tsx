f446e743480e3a63a089b4e7a96a98cb8cc89581
Ilias Doukas <doukas.lewis@gmail.com>
2025-02-07 13:56:40 +0200

    Create hacker room

---
+++ app/_sections/Hero.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../_components/HackerRoom";
+import CanvasLoader from "../_components/CanvasLoader";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "@/constants";
+
+const Hero = () => {
+  // Use Leva to position 3D object and positioning values manually
+  //   const x = useControls("HackerRoom", {
+  //     positionX: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     positionY: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     positionZ: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //   });
+
+  const isSmall = useMediaQuery({ maxWidth: 740 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  return (
+    <section className="min-h-screen w-full flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, I am Lewis <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Build Products & Brands</p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+              scale={sizes.deskScale}
+            />
+
+            <group></group>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Hero from "./_sections/Hero";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
